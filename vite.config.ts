875fb4125a920c1e12c619805674a05cfff5723c
xielaoshi <xielaoshi1999@foxmail.com>
2021-07-15 16:04:44 +0800

    update readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,35 @@
-require("dotenv").config({ path: join(__dirname, ".env") });
+require('dotenv').config({ path: join(__dirname, '.env') })
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { join } from "path";
-import electron from "vitejs-plugin-electron";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { join } from 'path'
+import electron from 'vitejs-plugin-electron'
 
-const root = join(__dirname, "src/render");
+const root = join(__dirname, 'src/render')
 
 export default defineConfig((env) => {
   return {
     plugins: [vue(), electron()],
     root,
-    base: "./", // index.html 中静态资源加载位置
+    base: './', // index.html 中静态资源加载位置
     server: {
       port: +process.env.PORT,
     },
     resolve: {
       alias: {
-        "@render": join(__dirname, "src/render"),
-        "@main": join(__dirname, "src/main"),
-        "@src": join(__dirname, "src"),
-        "@root": __dirname,
+        '@render': join(__dirname, 'src/render'),
+        '@main': join(__dirname, 'src/main'),
+        '@src': join(__dirname, 'src'),
+        '@root': __dirname,
       },
     },
     build: {
-      outDir: join(__dirname, "dist/render"),
+      outDir: join(__dirname, 'dist/render'),
       emptyOutDir: true,
       minify: false,
       commonjsOptions: {},
-      assetsDir: "", // 相对路径 加载问题
+      assetsDir: '', // 相对路径 加载问题
       sourcemap: false,
     },
-  };
-});
+  }
+})
