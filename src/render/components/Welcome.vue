971aa24e98f6a3852fe38ccb0d4c7e228f4c4847
xielaoshi <xielaoshi1999@foxmail.com>
2021-07-15 16:09:04 +0800

    UPDATE

--- src/render/components/Welcome.vue
+++ src/render/components/Welcome.vue
@@ -36,10 +36,10 @@
 </template>
 
 <script>
-  import axios from "axios";
-  import electron from "electron";
+  import axios from 'axios'
+  import electron from 'electron'
   export default {
-    name: "Welcome",
+    name: 'Welcome',
     props: {
       tabledata: Object,
       type: String,
@@ -47,76 +47,60 @@
     },
     data() {
       return {
-        yiYan: "",
-        date: "",
-        time: "",
-      };
+        yiYan: '',
+        date: '',
+        time: '',
+      }
     },
     created() {
-      this.getYiYan();
-      this.getTime();
+      this.getYiYan()
+      this.getTime()
     },
     methods: {
       async getYiYan() {
-        let res = await axios.get(`https://v1.hitokoto.cn/`);
-        console.log(res.data);
-        this.yiYan = res.data.hitokoto;
-        this.yiYanAuthor =
-          "——" + (res.data.from_who || "") + "《" + res.data.from + "》";
+        let res = await axios.get(`https://v1.hitokoto.cn/`)
+        console.log(res.data)
+        this.yiYan = res.data.hitokoto
+        this.yiYanAuthor = '——' + (res.data.from_who || '') + '《' + res.data.from + '》'
       },
       getTime() {
-        var weekday = new Array(7);
-        weekday[0] = "星期日";
-        weekday[1] = "星期一";
-        weekday[2] = "星期二";
-        weekday[3] = "星期三";
-        weekday[4] = "星期四";
-        weekday[5] = "星期五";
-        weekday[6] = "星期六";
+        var weekday = new Array(7)
+        weekday[0] = '星期日'
+        weekday[1] = '星期一'
+        weekday[2] = '星期二'
+        weekday[3] = '星期三'
+        weekday[4] = '星期四'
+        weekday[5] = '星期五'
+        weekday[6] = '星期六'
         setInterval(() => {
-          let nowDate = new Date();
-          let monthStr = "0" + (nowDate.getMonth() * 1 + 1);
-          let dayStr = "0" + nowDate.getDate();
-          let wd = weekday[nowDate.getDay()];
-          let hh = "0" + nowDate.getHours();
-          let mm = "0" + nowDate.getMinutes();
-          let ss = "0" + nowDate.getSeconds();
-          this.date =
-            nowDate.getFullYear() +
-            "年" +
-            this.shorter(monthStr) +
-            "月" +
-            this.shorter(dayStr) +
-            "日 " +
-            wd;
-          this.time =
-            this.shorter(hh) + ":" + this.shorter(mm) + ":" + this.shorter(ss);
-        }, 1000);
+          let nowDate = new Date()
+          let monthStr = '0' + (nowDate.getMonth() * 1 + 1)
+          let dayStr = '0' + nowDate.getDate()
+          let wd = weekday[nowDate.getDay()]
+          let hh = '0' + nowDate.getHours()
+          let mm = '0' + nowDate.getMinutes()
+          let ss = '0' + nowDate.getSeconds()
+          this.date = nowDate.getFullYear() + '年' + this.shorter(monthStr) + '月' + this.shorter(dayStr) + '日 ' + wd
+          this.time = this.shorter(hh) + ':' + this.shorter(mm) + ':' + this.shorter(ss)
+        }, 1000)
       },
       shorter(inStr) {
-        return inStr.substring(inStr.length - 2);
+        return inStr.substring(inStr.length - 2)
       },
       viewLib() {
         this.$notify.info({
-          title: "依赖版本",
+          title: '依赖版本',
           dangerouslyUseHTMLString: true,
-          message:
-            "<div>" +
-            "<p>vue：3.1.2</p>" +
-            "<p>element-plus：1.0.2</p>" +
-            "<p>electron：13.1.6</p>" +
-            "<p>Chromium：91.0.2</p>" +
-            "<p>Node.js：14.16.0</p>" +
-            "</div>",
-          position: "bottom-right",
-        });
+          message: '<div>' + '<p>vue：3.1.2</p>' + '<p>element-plus：1.0.2</p>' + '<p>electron：13.1.6</p>' + '<p>Chromium：91.0.2</p>' + '<p>Node.js：14.16.0</p>' + '</div>',
+          position: 'bottom-right',
+        })
       },
       gobaidu() {
-        const shell = electron.shell;
-        shell.openExternal("http://www.baidu.com");
+        const shell = electron.shell
+        shell.openExternal('http://www.baidu.com')
       },
     },
-  };
+  }
 </script>
 <style>
   .el-row {
